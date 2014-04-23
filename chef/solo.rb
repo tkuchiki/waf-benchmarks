8783c8887e0e40f2007e523409327067dc3a3e00
tkuchiki <kuchiki.taku@gmail.com>
2014-04-23 21:12:46 +0900

    Add roles and nodes.

--- chef/solo.rb
+++ chef/solo.rb
@@ -1,3 +1,4 @@
 file_cache_path Chef::Config[:file_cache_path]
 cookbook_path   "#{File.expand_path(File.dirname(__FILE__))}/cookbooks"
+role_path       "#{File.expand_path(File.dirname(__FILE__))}/roles"
 verbose_logging false
